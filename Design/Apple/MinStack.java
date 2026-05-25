8554455e5ae3717c5d147611d4a8aa1bc33f339e
Manga Rao Nandamudi <mangaraonandamudi@Mangas-MacBook-Pro.local>
2026-05-25 17:29:09 -0500

    Designing Min Stack to fetch the minimum element in the stack at any point

---
+++ Design/Apple/MinStack.java
@@ -0,0 +1,68 @@
+/*
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ */
+
+class MinStack {
+
+    Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if(stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int[] top = stack.peek();
+            if(top[1] < val) {
+                stack.push(new int[]{val, top[1]});
+            } else {
+                stack.push(new int[]{val, val});
+            }
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
